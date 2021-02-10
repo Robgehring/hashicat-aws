5cfd3c044197972061488f4b8c53e4e0c0543dbc
robgehring <robgehring@gmail.com>
2021-02-10 20:03:02 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -125,8 +125,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department=acme
-    Billable=true
+    Department="acme"
+    Billable="true"
   }
 }
 
